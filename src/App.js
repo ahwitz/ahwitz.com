48b1c5bca4ba1823d3487ffb19879db3e4b6191f
ahwitz <ahwitz@gmail.com>
2021-02-14 20:00:07 -0500

    pre-mobile cleanup

--- src/App.js
+++ src/App.js
@@ -31,6 +31,11 @@ export default class App extends React.Component
         <div className="banner">
           <p className="banner-title">ahwitz - Andrew Horwitz</p>
           <p className="banner-subtitle">composer/software engineer</p>
+          <p className="banner-links">
+            <span className="banner-link">Email: <a href="mailto:ahwitz@gmail.com" target="_blank" rel="noreferrer">ahwitz@gmail.com</a></span>
+            <span className="banner-link">LinkedIn: <a href="https://linkedin.com/in/ahwitz" target="_blank" rel="noreferrer">also ahwitz</a></span>
+            <span className="banner-link">Github: <a href="https://github.com/ahwitz" target="_blank" rel="noreferrer">not surprisingly, ahwitz</a></span>
+          </p>
         </div>
 
         <div className="pages" data-active-index={this.state.activeIndex}>
--- src/components/MusicPage.jsx
+++ src/components/MusicPage.jsx
@@ -26,7 +26,7 @@ export class MusicPage extends Page
           <div className="page-preface">
             <p>I haven&apos;t had a reason to update this page for a long time, but there&apos;s a very good chance it&apos;s why you&apos;re here.</p>
 
-            <p>Send me an email if you want a copy of any of these. I&apos;ll probably give it to you in exchange for asking for a recording if you ever make one. Only piece here that&apos;s formally published is the Sonatina for Baritone Saxophone, which has purchase information available below. Can also still try sending me an email about that one.</p>
+            <p>Send me an email if you want a copy of any of these. I&apos;ll probably ask for a recording in exchange if you ever make one. The only piece here that&apos;s formally published is the Sonatina for Baritone Saxophone, which has purchase information available below. Can also still try sending me an email about that one.</p>
           </div>
           {compsLocal}
         </div>
